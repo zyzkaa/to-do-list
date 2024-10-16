b1568871f7fbb51f50d600e380ac187f24d12e07
zyzka <zuziaswis@gmail.com>
2024-10-16 15:03:30 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 let entries = [];
 const listDiv = document.getElementById("listCont");
-// const addButton = document.getElementById("entryButton");
+const addButton = document.getElementById("entryButton");
 const input = document.getElementById("entryInput");
 const storageKey = "list elements";
+
+input.addEventListener("keypress", function(event) {
+    if(event.key == "Enter") {
+        event.preventDefault();
+        addEntry();
+    }
+});
+
+addButton.addEventListener('click', () => {
+    const icon = document.getElementById("addIcon");
+
+    const value = input.value;
+    if(!value) return;
+    icon.classList.add('animate');
+
+    icon.addEventListener('animationend', () => {
+        icon.classList.remove('animate');
+    })
+})
 
 function displayEntries(){
     listDiv.innerHTML = null;
@@ -15,15 +34,16 @@ function displayEntries(){
         entryText.className = "entryText"
 
         const delButton = document.createElement("button");
-        delButton.innerHTML = "delete";
+        delButton.innerHTML = "del";
         delButton.onclick = () => deleteEntry(idx);
+        delButton.className = "delButton";
 
         const settingsButton = document.createElement("button")
         settingsButton.innerHTML = "settings"
 
         entryDiv.appendChild(entryText);
         entryDiv.appendChild(delButton);
-        entryDiv.appendChild(settingsButton);
+        // entryDiv.appendChild(settingsButton);
         listDiv.appendChild(entryDiv);
     }
 }
@@ -39,16 +59,22 @@ function addEntry(){
     if(!value) return;
 
     entries.push(value);
-    input.value = "";
-    displayEntries();
-    saveEntries();
+    input.classList.add('animate');
+    addButton.disabled = true;
+
+    setTimeout(() => {
+        input.value = "";
+        displayEntries();
+        saveEntries();
+        input.classList.remove('animate');
+        addButton.disabled = false;
+    }, 600);
 }
 
 function saveEntries(){
     const stringEntries = JSON.stringify(entries);
     localStorage.setItem(storageKey, stringEntries);
 }
-
 
 document.addEventListener("DOMContentLoaded", () => {
     const oldEntries = localStorage.getItem(storageKey);
